87f3a1d80d127eb1bea04c8cc76dcb04ca9e97c5
ysho <ysho2@illinois.edu>
2020-06-19 12:11:31 -0500

    Extracted info of all players from EPL.

--- main_file.py
+++ main_file.py
@@ -1,40 +1,140 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
+# import pandas as pd
+from os.path import basename
+
 
 
 headers = {'User-Agent':
            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
 
-url = "https://www.transfermarkt.us/premier-league/marktwerte/wettbewerb/GB1/pos//detailpos/0/altersklasse/alle/plus/1"
+url = "https://www.transfermarkt.co.uk/premier-league/startseite/wettbewerb/GB1"
 
-pageTree = requests.get(url, headers=headers)
-pageSoup = BeautifulSoup(pageTree.content, 'html.parser')
+tree = requests.get(url, headers=headers)
+soup = BeautifulSoup(tree.content, 'html.parser')
 
-table1 = pageSoup.find("table", attrs = {"class" : "items"})
-table_header = table1.thead.find_all("tr")
+# Create an empty list to assign these values to
+teamLinks = []
+
+# Extract all links with the correct CSS selector
+links = soup.select("a.vereinprofil_tooltip")
+
+# We need the location that the link is pointing to, so for each link, take the link location.
+# Additionally, we only need the links in locations 1,3,5,etc. of our list, so loop through those only
+for i in range(1, 41, 2):
+    teamLinks.append(links[i].get("href"))
+
+# For each location that we have taken, add the website before it - this allows us to call it later
+for i in range(len(teamLinks)):
+    teamLinks[i] = "https://www.transfermarkt.co.uk" + teamLinks[i]
+
+playerLinks = []
+newPlayerLinks = []
+
+for i in range(len(teamLinks)):
+    page = teamLinks[i]
+    tree = requests.get(page, headers=headers)
+    soup = BeautifulSoup(tree.content, 'html.parser')
+
+    # Extract all links
+    links = soup.select("a.spielprofil_tooltip")
+
+    # For each link, extract the location that it is pointing to
+    for j in range(len(links)):
+        playerLinks.append(links[j].get("href"))
+
+    # Add the location to the end of the transfermarkt domain to make it ready to scrape
+for j in range(len(playerLinks)):
+    playerLinks[j] = "https://www.transfermarkt.co.uk"+ playerLinks[j]
+
+# The page list the players more than once - let's use list(set(XXX)) to remove the duplicates
+playerLinks = list(set(playerLinks))
 
 
+# page = playerLinks[0]
+# tree = requests.get(page, headers=headers)
+# soup = BeautifulSoup(tree.content, 'html.parser')
+#
+# name = soup.find_all("h1")
+# value = soup.select("span.waehrung")
+# value1 = soup.find_all("span", class_="hauptpunkt")
+# print(value1[0].a.text)
 
-headings = []
-for th in table_header[0].find_all("th"):
-    headings.append(th.text.replace('\n', ' ').strip())
-
-data = []
-table_data = table1.tbody.find_all("tr")
-for tr in table_data:
-    t_row = {}
-    for td, th in zip(tr.find_all("td"), headings):
-        t_row[th] = td.text.replace('\n', '').strip()
-    data.append(t_row)
-
-
-print(data)
+# print(value1)
+# print(value[0].text)
+# print(value[0].next_sibling)
+# print(value[1].text)
 
 
-# print(headings)
-# PlayerList = []
-# ValuesList = []
+# tempValue = soup.find_all("div", class_="dataMarktwert")
+# print(tempValue[0])
+# print(tempValue[0].a.span.text)
+# print(tempValue[0].a.span.next_sibling)
+# print(tempValue[0].a.span.next_sibling.next_sibling.text)
 
+nameList = []
+club = []
+currency = []
+value = []
+denom = []
+total_value = []
 
+for i in range(len(playerLinks)):
+    page = playerLinks[i]
+    tree = requests.get(page, headers=headers)
+    soup = BeautifulSoup(tree.content, 'html.parser')
 
+    try:
+        tempValue = soup.find_all("div", class_="dataMarktwert")
+        currency.append(tempValue[0].a.span.text)
+    except IndexError:
+        continue
+    try:
+        tempValue = soup.find_all("div", class_="dataMarktwert")
+        value.append(float(tempValue[0].a.span.next_sibling))
+    except IndexError:
+        currency.pop()
+        continue
+
+    try:
+        tempValue = soup.find_all("div", class_="dataMarktwert")
+        denom.append(tempValue[0].a.span.next_sibling.next_sibling.text)
+    except IndexError:
+        currency.pop()
+        value.pop()
+        continue
+
+    try:
+        tempValue = soup.find_all("div", class_="dataMarktwert")
+        total_value.append(tempValue[0].a.span.text + str(tempValue[0].a.span.next_sibling) + tempValue[0].a.span.next_sibling.next_sibling.text)
+    except IndexError:
+        currency.pop()
+        value.pop()
+        denom.pop()
+        continue
+    try:
+        tempClub = soup.find_all("span", class_="hauptpunkt")
+        club.append(tempClub[0].a.text)
+    except IndexError:
+        currency.pop()
+        value.pop()
+        denom.pop()
+        total_value.pop()
+        continue
+    try:
+        name = soup.find_all("h1")
+        nameList.append(name[0].text)
+    except IndexError:
+        currency.pop()
+        value.pop()
+        denom.pop()
+        total_value.pop()
+        club.pop()
+        continue
+
+print(len(nameList))
+print(len(club))
+print(len(currency))
+print(len(value))
+print(len(denom))
+print(len(total_value))
